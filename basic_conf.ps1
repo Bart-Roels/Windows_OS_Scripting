ac62820651026aead3494233a1a6c3526f02d8cf
bart-roels <bart.roels@student.howest.be>
2023-02-28 17:29:26 +0100

    dc: rename files & start testing

---
+++ basic_conf.ps1
@@ -0,0 +1,44 @@
+# Prompt for the hostname 
+$hostname = Read-Host "Enter the hostname for this server (e.g. FileServer01)"
+
+# Prompt for the IP address and subnet mask
+$ipAddress = Read-Host "Enter the IP address for this server (e.g. 192.168.0.10)"
+$subnetMask = Read-Host "Enter the subnet mask for this server (e.g. 255.255.255.0)"
+$defaultGateway = Read-Host "Enter the default gateway for this server"
+$dnsServers = Read-Host "Enter the DNS servers for this server (e.g. '8.8.8.8', '8.8.4.4')"
+
+# Set the hostname to the specified value
+Rename-Computer -NewName $hostname -Restart
+
+# Configure a static IP address
+$nicParams = @{
+    IPAddress      = $ipAddress
+    PrefixLength   = $subnetMask
+    DefaultGateway = $defaultGateway
+}
+Get-NetAdapter -Name "Ethernet" | Set-NetIPAddress @nicParams
+Get-NetAdapter -Name "Ethernet" | Set-DnsClientServerAddress -ServerAddresses $dnsServers
+
+# Choose the timezone
+Set-TimeZone -Name "Central European Standard Time"
+
+
+# Enable remote desktop access
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -Name "fDenyTSConnections" -Value 0
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+
+# Disable IE Enhanced Security Setting
+$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+$ieParams = @{
+    Path  = $AdminKey
+    Name  = "IsInstalled"
+    Value = 0
+}
+Set-ItemProperty @ieParams
+$ieParams.Path = $UserKey
+Set-ItemProperty @ieParams
+Stop-Process -Name Explorer
+
+# Set the Control Panel view to Small icons
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel" -Name "AllItemsIconView" -Value 
