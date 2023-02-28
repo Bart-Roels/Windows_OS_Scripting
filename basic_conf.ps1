1a93c57123c569b0d4fd22d109301b04b278cf0e
bart-roels <bart.roels@student.howest.be>
2023-02-28 18:39:14 +0100

    Fix: error on gateway

--- basic_conf.ps1
+++ basic_conf.ps1
@@ -1,24 +1,16 @@
-# Prompt for the hostname 
-$hostname = Read-Host "Enter the hostname for this server (e.g. FileServer01)"
-# Prompt for the IP address and subnet mask
+$adapter = Read-Host "Enter the name of the network adapter you want to configure"
 $ipAddress = Read-Host "Enter the IP address for this server (e.g. 192.168.0.10)"
 $subnetMask = Read-Host "Enter the subnet mask for this server (e.g. 255.255.255.0)"
 $defaultGateway = Read-Host "Enter the default gateway for this server"
-$dnsServers = Read-Host "Enter the DNS servers for this server (e.g. '8.8.8.8', '8.8.4.4')"
+$dnsServers = Read-Host "Enter the DNS server addresses (separated by commas)"
 
+$prefixLength = Convert-NetmaskToPrefixLength $subnetMask
 
-# Configure a static IP address
-$nicParams = @{
-    IPAddress      = $ipAddress
-    PrefixLength   = $subnetMask
-    DefaultGateway = $defaultGateway
-}
-Get-NetAdapter -Name "Ethernet0" | Set-NetIPAddress @nicParams
-Get-NetAdapter -Name "Ethernet0" | Set-DnsClientServerAddress -ServerAddresses $dnsServers
+New-NetIPAddress -InterfaceAlias $adapter -IPAddress $ipAddress -PrefixLength $prefixLength -DefaultGateway $defaultGateway
+Set-DnsClientServerAddress -InterfaceAlias $adapter -ServerAddresses $dnsServers.Split(',')
 
 # Choose the timezone
 Set-TimeZone -Name "Central European Standard Time"
-
 
 # Enable remote desktop access
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -Name "fDenyTSConnections" -Value 0
@@ -38,12 +30,13 @@ Set-ItemProperty @ieParams
 Stop-Process -Name Explorer
 
 # Set the Control Panel view to Small icons
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel" -Name "AllItemsIconView" -Value 
-
-# Set the hostname to the specified value
-Rename-Computer -NewName $hostname
+If (!(Test-Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel")) {
+    New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel" | Out-Null
+}
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel" -Name "StartupPage" -Type DWord -Value 1
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel" -Name "AllItemsIconView" -Type DWord -Value 1
 
 # Notify user and restart after 10 seconds
-Write-Host "The server will restart in 10 seconds."
-Start-Sleep -Seconds 10
-Restart-Computer -Force
+# Write-Host "The server will restart in 10 seconds."
+# Start-Sleep -Seconds 10
+# Restart-Computer -Force
