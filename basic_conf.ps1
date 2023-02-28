712d9a2c7590b3a5f4032d58c0025d38170fc19c
bart-roels <bart.roels@student.howest.be>
2023-02-28 19:03:10 +0100

    Fix: fixing bugs

--- basic_conf.ps1
+++ basic_conf.ps1
@@ -1,23 +1,27 @@
+# Hostname
+$ComputerName = Read-Host "Enter the computer name (e.g. server1)"
+
 # Prompt user for input
 $IPAddress = Read-Host "Enter IP address (e.g. 192.168.1.100)"
-$SubnetMask = Read-Host "Enter subnet mask (e.g. 255.255.255.0)"
+$Prefix = Read-Host "Enter subnet prefix (e.g. 24)"
 $Gateway = Read-Host "Enter default gateway (e.g. 192.168.1.1)"
 $InterfaceAlias = Read-Host "Enter interface alias (e.g. Ethernet)"
-
-# Convert subnet mask to byte value and set IPv4 network settings using the input
-$PrefixLength = (ConvertTo-Byte -InputObject $SubnetMask.Split(".")[-1])
-Set-NetIPAddress -InterfaceAlias $InterfaceAlias -IPAddress $IPAddress -PrefixLength $PrefixLength -DefaultGateway $Gateway
-
-# Set interface operational status and DNS server addresses using the input
-Set-NetIPInterface -InterfaceAlias $InterfaceAlias -InterfaceOperationalStatus Up ` -DnsServerAddresses ($DNSServers -split ',')
+$dnsServer1 = Read-Host "Enter the primary DNS server for this server"
+$dnsServer2 = Read-Host "Enter the secondary DNS server for this server"
 
 
+# Set IPv4 network settings using the input
+New-NetIPAddress –IPAddress $IPAddress  -DefaultGateway $Gateway -PrefixLength $Prefix -InterfaceIndex (Get-NetAdapter).InterfaceIndex
 
-# Disable ipv6
-Set-NetIPInterface -InterfaceAlias $adapter -Forwarding Enabled -Dhcp Enabled -AddressFamily IPv6 -InterfaceMetric 1 -InterfaceIndex 1 -InterfaceOperationalStatus Up -InterfaceAlias $adapter -InterfaceDescription $adapter -InterfaceT
+# Set DNS server addresses using the input
+Set-DNSClientServerAddress –InterfaceIndex (Get-NetAdapter).InterfaceIndex –ServerAddresses $dnsServer1, $dnsServer2
+
+# Disable IPv6
+Set-NetIPInterface -InterfaceAlias $InterfaceAlias -Forwarding Enabled -AddressFamily IPv6 -InterfaceMetric 1
 
 # Choose the timezone
-Set-TimeZone -Name "Central European Standard Time"
+Set-TimeZone -Name 'Central European Standard Time' -PassThru
+
 
 # Enable remote desktop access
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -Name "fDenyTSConnections" -Value 0
@@ -43,7 +47,15 @@ If (!(Test-Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Contro
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel" -Name "StartupPage" -Type DWord -Value 1
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel" -Name "AllItemsIconView" -Type DWord -Value 1
 
-# Notify user and restart after 10 seconds
-# Write-Host "The server will restart in 10 seconds."
-# Start-Sleep -Seconds 10
-# Restart-Computer -Force
+# Set in file explorer to show hidden files
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Hidden" -Type DWord -Value 1
+# Set in file explorer to show file extensions
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Type DWord -Value 0
+
+# Set hostname
+Rename-Computer -NewName $ComputerName 
+# Warn user that the computer needs to be restarted
+Write-Host "The computer needs to be restarted to apply the changes. Press any key to restart the computer."
+$null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+Restart-Computer -Force
+
