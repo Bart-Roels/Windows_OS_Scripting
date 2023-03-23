252a5845b9a4ad350f14e51217d9cc4715a39c8d
naam studentc:/Users/bart-/OneDrive/Documenten/Repo's/FSWD-Projecten/venv_fswd/Scripts/python.exe <bart.roels@student.howest.be>
2023-03-23 12:12:41 +0100

    Fix: UPN fix

--- Opdracht_3/DC1_UPN_suffix.ps1
+++ Opdracht_3/DC1_UPN_suffix.ps1
@@ -1,11 +1,22 @@
 # Import the Active Directory module
 Import-Module ActiveDirectory
+Add-Type -AssemblyName Microsoft.VisualBasic
 
-if(Get-ADForrest | where-Object{ $_.UPNSuffixes -match $UPN_suffix })
-{
+# Domain name
+$UPN_suffix = [Microsoft.VisualBasic.Interaction]::InputBox("Enter the UPN suffix (e.g. intranet.mycompany.be)", "UPN suffix")
+
+# Check if the UPN suffix already exists
+if (Get-ADForest | where-Object { $_.UPNSuffixes -match $UPN_suffix }) {
     Write-Host "UPN suffix already exists"
 }
 else {
-    Get-ADForrest | Set-ADForrest -UPNSuffixes $UPN_suffix
-    
+    # create a hashtable with the UPN suffix 
+    $upnHashtable = @{
+        Add = $UPN_suffix
+    }
+    Get-ADForest | Set-ADForest -UPNSuffixes $upnHashtable
 }
+
+# Print script is finished
+Write-Host "Script is finished" -ForegroundColor Green
+
