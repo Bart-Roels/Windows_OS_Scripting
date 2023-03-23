3095980cec368e8aa5573be2ad2ee027f7d1d17b
naam studentc:/Users/bart-/OneDrive/Documenten/Repo's/FSWD-Projecten/venv_fswd/Scripts/python.exe <bart.roels@student.howest.be>
2023-03-23 12:26:54 +0100

    Fix: profile share fixes

--- Opdracht_3/ProfileShare.ps1
+++ Opdracht_3/ProfileShare.ps1
@@ -2,17 +2,16 @@
 # DC 2 
 
 # Fileserver
-$fileserver = "WIN17-MS"
-
+$serverName = "WIN17-MS"
 $SystemShare = "C$"
-$drive = $SystemShare.replace("$",":")
+$drive = $SystemShare.replace("$", ":")
 $shareName = "Profiles"
-$localPath = "$drive"+ "\" + $shareName
-$path = "\\" + $serverName + "\" +$SystemShare + "\" + $shareName
+$localPath = "$drive" + "\" + $shareName
+$path = "\\" + $serverName + "\" + $SystemShare + "\" + $shareName
 
 
 # Check if path exists
-if(Test-Path $path) {
+if (Test-Path $path) {
     Write-Host "The share already exists on the server"
     return
 }
@@ -23,22 +22,22 @@ else {
 }
 
 # Check if the share already exists
-if(Get-SmbShare -CimSession $serverName -Name $shareName -ErrorAction SilentlyContinue) {
+if (Get-SmbShare -CimSession $serverName -Name $shareName -ErrorAction SilentlyContinue) {
     Write-Host "The share already exists on the server"
     return
 }
 else {
     Write-Host "The share does not exist on the server"
-    Write-Host "Creating the share on the server"
-    New-SmbShare -CimSession $serverName -Name $shareName -Path $path -FullAccess "Everyone"  | Out-Null
+    Write-Host "Creating the share on the server, shearing $localPath to $shareName on $serverName as $SystemShare "
+    New-SmbShare -CimSession $serverName -Name $shareName -Path $localPath -FullAccess "Everyone"  | Out-Null
 }
 
 # Set the ACL permissions for the share folder
 $ACL = Get-Acl $path
 # Disable inheritance and remove all inherited permissions
-$ACL.SetAccessRuleProtection($true,$false)
+$ACL.SetAccessRuleProtection($true, $false)
 # Settings full control for the Administrators --> Container Inherit, Object Inherit
-$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("Administrators","FullControl","ContainerInherit,ObjectInherit","None","Allow")
+$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("Administrators", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")
 $ACL.SetAccessRule($AccessRule)
 
 # 
