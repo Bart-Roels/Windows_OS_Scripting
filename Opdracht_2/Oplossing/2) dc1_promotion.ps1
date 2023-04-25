ee83f8ef33699585db0f6df27ea001888fa52d65
bart-roels <bart.roels@student.howest.be>
2023-04-25 10:23:10 +0200

    Fix: added sollution

---
+++ Opdracht_2/Oplossing/2) dc1_promotion.ps1
@@ -0,0 +1,47 @@
+Add-Type -AssemblyName Microsoft.VisualBasic
+
+# Domain name
+$domainName = [Microsoft.VisualBasic.Interaction]::InputBox("Enter the domain name (e.g. intranet.mycompany.be)", "Domain name")
+
+try {
+
+    # Check if the necessary role(s) is/are installed. If not, install them.
+    $roles = "AD-Domain-Services", "DNS"
+    foreach ($role in $roles) {
+        if ((Get-WindowsFeature -Name $role).Installed -ne $true) {
+            Install-WindowsFeature -Name $role -IncludeManagementTools
+            # Installing background green
+            Write-Host "The $role role has been installed." -BackgroundColor Green
+        }
+        else {
+            # Alread installed forgrond orange
+            Write-Host "The $role role is already installed." -ForegroundColor Yellow
+        }
+    }
+
+
+    #
+    # FORREST
+    #
+
+    # Make netbios name from domain name take middle part
+    $domainNetbiosName = $domainName.Split(".")[1]
+    # Make sure the netbios name is not longer than 15 characters
+    if ($domainNetbiosName.Length -gt 15) {
+        $domainNetbiosName = $domainNetbiosName.Substring(0, 15)
+    }
+    # Set the domain netbios name to uppercase
+    $domainNetbiosName = $domainNetbiosName.ToUpper()
+
+
+    # Promote the first server to the first DC for the new forest/domain no reboot
+    Install-ADDSForest -DomainName $domainName -DomainNetbiosName $domainNetbiosName -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText "P@ssw0rd" -Force) -NoRebootOnCompletion
+
+
+}
+catch {
+    Write-Host "An error occurred while configuring the domain: $($Error[0].Exception.Message)" -ForegroundColor Red
+}
+finally {
+    Write-Host "Domain configuration complete." -ForegroundColor Green
+}
